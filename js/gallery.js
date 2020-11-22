91d4e0bdffb82e5f718c536e68ff5b685dc5aa9a
ValeriiMatviienko <unanimous276@gmail.com>
2020-11-22 14:37:11 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -6,8 +6,6 @@ const modalImg = document.querySelector(".lightbox__image");
 const modalContent = document.querySelector(".lightbox__image");
 const overlay = document.querySelector(".lightbox__overlay")
 const modalBtnClose = document.querySelector(".lightbox__button");
-const modalBtnRight = document.querySelector(".scroll-right");
-const modalBtnLeft = document.querySelector(".scroll-left");
 
 galleryContainer.addEventListener('click', modalOpen);
 galleryContainer.insertAdjacentHTML("beforeend", galleryCardMarkup(galleryItems));
@@ -42,8 +40,6 @@ function modalOpen(event) {
     document.addEventListener("keydown", modalCloseByEsc);
     modalBtnClose.addEventListener('click', modalClose);
     window.addEventListener("keydown", modalImgScrolling);
-    modalBtnRight.addEventListener("click", modalImgScrolling);
-    modalBtnLeft.addEventListener("click", modalImgScrolling);
     modalContent.addEventListener("click", modalImgScrolling);   
 };
 
@@ -55,8 +51,6 @@ function modalClose(event) {
     document.removeEventListener("keydown", modalCloseByEsc);
     modalBtnClose.removeEventListener('click', modalClose);
     window.removeEventListener("keydown", modalImgScrolling);
-    modalBtnRight.removeEventListener("click", modalImgScrolling);
-    modalBtnLeft.removeEventListener("click", modalImgScrolling);
     modalContent.removeEventListener("click", modalImgScrolling);
 };
 
@@ -82,13 +76,13 @@ function modalImgScrolling(event) {
 
     let imgIndex = galleryItems.findIndex(img => img.original === modalImg.src);
 
-    if (event.code === 'ArrowLeft' || event.code === 'ArrowDown' || modalBtnLeft === event.target) {
+    if (event.code === 'ArrowLeft' || event.code === 'ArrowDown') {
         if (imgIndex === 0) {
         imgIndex += galleryItems.length;}
         imgIndex -= 1;
     };
 
-    if (event.code === 'ArrowRight' || event.code === 'ArrowUp' || modalBtnRight === event.target || modalContent === event.target) {
+    if (event.code === 'ArrowRight' || event.code === 'ArrowUp' || modalContent === event.target) {
         if (imgIndex === galleryItems.length - 1) {
         imgIndex -= galleryItems.length;}
         imgIndex += 1;
